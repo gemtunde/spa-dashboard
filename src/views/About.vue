72e3998b285404f9a6ccf4b2e6e74de4d1ff11dc
gemtunde <gemtunde@gmail.com>
2023-03-29 17:31:42 +0100

    intial commit

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="about-page">
+        <h2>About Page</h2>
+        <p>This is about page</p>
+    </main>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <main class="home-page">
+      <h2>Home Page</h2>
+      <p>This is home page</p>
+    </main>
+</template>
